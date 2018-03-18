4dd12df8dde35cfa04b4d0acdb2fe8514d8f2398
trungbanh <trungbanh97>
2018-03-18 15:42:12 +0700

    update data

---
+++ app/src/main/java/com/android/trungbanh/guess_the_guy/Friend.java
@@ -0,0 +1,21 @@
+package com.android.trungbanh.guess_the_guy;
+
+/**
+ * Created by trungbanh on 3/18/18.
+ */
+
+public class Friend {
+
+    public Friend(String name,String sdt ,String conviec , String ghi_chu) {
+        this.name =name ;
+        this.sdt = sdt ;
+        this.conviec =conviec;
+        this.ghi_chu =ghi_chu ;
+    }
+
+    String name ;
+    String sdt;
+    String conviec ;
+    String ghi_chu;
+}
+
